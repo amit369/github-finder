f397198451239f13b4d4e4d137512d35cffd10ee
amit369 <amitchaman369@gmail.com>
2023-02-11 10:51:02 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes}  from 'react-router-dom'; 
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" elnt={<Home/>} /> */}
+          <Route exact path="/about" element={<About/>} /> 
+          {/* <Route exact path="/user/:login" element={<User/>} /> */}
+          <Route element={<NotFound />} />
+            </Routes>
+     </Router>
     </div>
   );
 }
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NotFound () 
+{
+    return(
+        <div>
+            <h1>Not Found </h1>
+            <p classname="lead">This is not the page you're looking for </p>
+
+            </div>
+    );
+    
+}
+
+export default NotFound;
